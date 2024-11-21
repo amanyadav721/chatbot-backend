b86c9a3c900844a26563739da563ebd176f3a589
amankumar <ad721603@gmail.com>
2024-11-21 19:39:43 +0530

    feat:portadded

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-
+import os
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Render!"}
 
-@app.get('/')
-def hello_world():
-    return "Hello,World"
-
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Use PORT from the environment, default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
